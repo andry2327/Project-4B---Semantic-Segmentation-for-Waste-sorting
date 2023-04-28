89a5555126683d1b7ea77a601c0dd0ce0fd8639c
Lorenzo Tozzi <60443037+anubis09@users.noreply.github.com>
2023-04-28 14:35:35 +0200

    Update config.py

--- project-WasteSemSeg/config.py
+++ project-WasteSemSeg/config.py
@@ -18,7 +18,7 @@ __C.VIS = edict()
 #------------------------------DATA------------------------
 
 __C.DATA.DATASET = 'city' # dataset
-__C.DATA.DATA_PATH = '/home/shyam/course_project/synthetic_single/'
+__C.DATA.DATA_PATH = '/content/dataset'
 __C.DATA.NUM_CLASSES = 1
 __C.DATA.IGNORE_LABEL = 255
 __C.DATA.IGNORE_LABEL_TO_TRAIN_ID = 19 # 255->19
