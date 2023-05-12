5e9c0d5d4d1cb5d6e0e9e821e05158b39c9a8ace
andry2327 <andry2327@gmail.com>
2023-05-12 11:15:51 +0200

    minor fix in plot

--- project-WasteSemSeg/utils.py
+++ project-WasteSemSeg/utils.py
@@ -99,4 +99,4 @@ def plot_mIoU_validation(N_epoch, mIoU_list):
     print(f'x = {N_epoch}, y = {len(mIoU_list)}')
 
     plt.title(f'mean IoU')
-    plt.plot(list(range(N_epoch)), mIoU_list)
+    plt.plot([x+1 for x in range(N_epoch)], mIoU_list)
