ed048764c7099797a79b8e03d4c657863e19a9e1
andry2327 <andry2327@gmail.com>
2023-05-16 21:39:21 +0200

    fix

--- project-WasteSemSeg/train.py
+++ project-WasteSemSeg/train.py
@@ -112,7 +112,7 @@ def main(net_name = 'Enet', checkpoint = False):
             }
             torch.save(checkpoint, f'checkpoints/{net_name}/checkpoint_{net_name}_epoch={epoch+1}.pth')
             print(f"Model checkpoint '{f'checkpoints/{net_name}/checkpoint_{net_name}_epoch={epoch+1}.pth'}' saved")
-            if epoch+1 >= save_every:
+            if epoch >= save_every:
                 os.remove(f'checkpoints/{net_name}/checkpoint_{net_name}_epoch={epoch+1-save_every}.pth')
 
     return mIoU_list
