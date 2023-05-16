2cffff79c62b2ba1f1dba8f2b9be555543ff6038
andry2327 <andry2327@gmail.com>
2023-05-16 13:01:00 +0200

    model load fix

--- project-WasteSemSeg/train.py
+++ project-WasteSemSeg/train.py
@@ -80,7 +80,7 @@ def main(net_name = 'Enet'):
     if len(os.listdir(f'checkpoints/{net_name}')) > 1:
         # load the saved checkpoint
         path_pth_file = [file for file in os.listdir(f'checkpoints/{net_name}') if '.pth' in file][0]
-        checkpoint = torch.load(path_pth_file)
+        checkpoint = torch.load(f'checkpoints/{net_name}/{path_pth_file}')
 
         # restore the state of the model and optimizer
         net.load_state_dict(checkpoint['model_state_dict'])
