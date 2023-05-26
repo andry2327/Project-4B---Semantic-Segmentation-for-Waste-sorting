258815b9c74874b8d06bd8b93d25a920a0052ab1
andry2327 <andry2327@gmail.com>
2023-05-26 17:45:02 +0200

    added  N_CLAS={cfg.DATA.NUM_CLASSES} to checkpoint

--- project-WasteSemSeg/train.py
+++ project-WasteSemSeg/train.py
@@ -113,8 +113,8 @@ def main(net_name = 'Enet', checkpoint = False):
                 'epoch': epoch+1,
                 'mIoU_list': mIoU_list
             }
-            torch.save(checkpoint, f'checkpoints/{net_name}/checkpoint_{net_name}_epoch={epoch+1}.pth')
-            print(f"🔷 Model checkpoint '{f'checkpoint_{net_name}_epoch={epoch+1}.pth'}' saved")
+            torch.save(checkpoint, f'checkpoints/{net_name}/checkpoint_{net_name}_N_CLASSES={cfg.DATA.NUM_CLASSES}_epoch={epoch+1}.pth')
+            print(f"🔷 Model checkpoint '{f'checkpoint_{net_name}_N_CLASSES={cfg.DATA.NUM_CLASSES}_epoch={epoch+1}.pth'}' saved")
             if epoch >= start_epoch+save_every:
                 os.remove(f'checkpoints/{net_name}/checkpoint_{net_name}_epoch={epoch+1-save_every}.pth')
 
