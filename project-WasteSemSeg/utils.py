80de05a9408f77a5904a7aea304b2ea1de68e1d4
anubis09 <lorenzotozzi98@gmail.com>
2023-05-26 11:33:39 +0200

    debug

--- project-WasteSemSeg/utils.py
+++ project-WasteSemSeg/utils.py
@@ -27,9 +27,13 @@ def adjust_learning_rate(lr, decay, optimizer, cur_epoch, n_epochs):
 
 def calculate_mean_iu(predictions, gts, num_classes):
     sum_iu = 0
+    print("pred_Shape: ", predictions.shape)
+    print("gts_shape: ",gts.shape)
     for i in range(num_classes):
         n_ii = t_i = sum_n_ji = 1e-9
         for p, gt in zip(predictions, gts):
+            print("p_Shape: ", p.shape)
+            print("gt_shape: ",gt.shape)
             n_ii += np.sum(gt[p == i] == i)
             t_i += np.sum(gt == i)
             sum_n_ji += np.sum(p == i)
