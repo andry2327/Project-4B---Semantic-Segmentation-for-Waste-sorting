5272580bb2d23eaac7fd50a59dfccae0bc1f9118
andry2327 <andry2327@gmail.com>
2023-06-13 11:21:16 +0200

    DEBUG

--- project-WasteSemSeg/model.py
+++ project-WasteSemSeg/model.py
@@ -157,6 +157,7 @@ class BottleNeck(nn.Module):
         other_net = nn.Sequential(self.block1x1_1, self.middle_block,
                                   self.block1x1_2)
         other = other_net(input)
+        print(f'main.shape = {main.shape}')
         output = F.relu(main + other)
         if (self.downsampling):
             return output, indices
